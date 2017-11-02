fc165f5c2fe687c8e14fcf98e471338380992e88
Dmitry Basov <dmbasov@gmail.com>
2017-11-02 13:46:39 +0700

    skip ui.chart.lockNonSelected validation if 'splash' (#20)

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ export default Vizabi.Tool.extend("BubbleChart", {
 
     this._super(model);
 
-    if (model.ui.chart.lockNonSelected) {
+    if (model.ui.chart.lockNonSelected && (!model.ui.splash || model.state.time.splash === false)) {
       const time = model.state.time.parse("" + model.ui.chart.lockNonSelected);
       if (time < model.state.time.start) model.ui.chart.lockNonSelected = model.state.time.formatDate(model.state.time.start);
       if (time > model.state.time.end) model.ui.chart.lockNonSelected = model.state.time.formatDate(model.state.time.end);
