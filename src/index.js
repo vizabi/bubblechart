d18b564e1ce07f126f2e676799e39db4ca6917e2
Angie <konzeptmeister@gmail.com>
2021-03-01 09:34:39 +0100

    Remove old implementation

--- src/index.js
+++ src/index.js
@@ -133,58 +133,8 @@ BubbleChart.DEFAULT_UI = {
 
 
 
-
-//export default 
-const _BubbleChart = {
-
-  /**
-   * Initializes the tool (Bubble Chart Tool).
-   * Executed once before any template is rendered.
-   * @param {Object} placeholder Placeholder element for the tool
-   * @param {Object} external_model Model as given by the external page
-   */
-  init(placeholder, external_model) {
-
-    this.name = "bubblechart";
-
-    //specifying components
-    this.components = [{
-      component,
-      placeholder: ".vzb-tool-viz",
-      model: ["state.time", "state.marker", "locale", "ui"] //pass models to component
-    }, {
-      component: Vizabi.Component.get("timeslider"),
-      placeholder: ".vzb-tool-timeslider",
-      model: ["state.time", "state.marker", "ui"]
-    }, {
-      component: Vizabi.Component.get("dialogs"),
-      placeholder: ".vzb-tool-dialogs",
-      model: ["state", "ui", "locale"]
-    }, {
-      component: Vizabi.Component.get("buttonlist"),
-      placeholder: ".vzb-tool-buttonlist",
-      model: ["state", "ui", "locale"]
-    }, {
-      component: Vizabi.Component.get("treemenu"),
-      placeholder: ".vzb-tool-treemenu",
-      model: ["state.marker", "state.time", "locale", "ui"]
-    }, {
-      component: Vizabi.Component.get("datawarning"),
-      placeholder: ".vzb-tool-datawarning",
-      model: ["locale"]
-    }, {
-      component: Vizabi.Component.get("datanotes"),
-      placeholder: ".vzb-tool-datanotes",
-      model: ["state.marker", "locale"]
-    }, {
-      component: Vizabi.Component.get("steppedspeedslider"),
-      placeholder: ".vzb-tool-stepped-speed-slider",
-      model: ["state.time", "locale"]
-    }];
-
-    this._super(placeholder, external_model);
-
-  },
+ 
+const OldBubbleChart = {
 
   validate(model) {
     model = this.model || model;
